0dfd563c9534470622208bb0025876564224e849
hellsdeur <mtshelder@gmail.com>
2023-06-24 21:43:03 -0300

    Regex

--- main.cpp
+++ main.cpp
@@ -9,15 +9,27 @@ int main(int argc, char* argv[]) {
         text_filename = argv[1];
         regex_filename = argv[2];
     } else {
-        std::cout << "Please, enter a text file and a regex file as parameters\n";
+        std::cout << "Please, enter a text file and a regex file as parameters.\n";
         return 0;
     }
 
     Table table = Table(text_filename, regex_filename);
 
-    table.print_regex_maps();
+//    table.print_regex_maps();
 
-    table.print_text(0, 10);
+//    table.print_text(0, 10);
+
+    for (int i = 0; i < table.r; i++) {
+        for (int j = 1; j < table.c; j++) {
+            table.data[j][i] = table.match(table.data[0][i], table.regex[j-1]);
+        }
+    }
+
+//    for (int i = 0; i < table.c; i++) {
+//        std::cout << table.columns[i] << "\n";
+//    }
+
+    table.write_csv("./data/result.csv");
 
     return 0;
 }
--- src/table.h
+++ src/table.h
@@ -4,15 +4,26 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <regex>
+#include <omp.h>
+
+#define THREAD_NUM 4
 
 #define MAX_ROWS 8192
 #define MAX_COLS 24
 
 class Table {
-private:
+public:
+    Table(std::string text_filename, std::string regex_filename) {
+        this->text_filename = text_filename;
+        this->regex_filename = regex_filename;
+        load_text();
+        load_regex();
+    }
+
     std::string text_filename;
     std::string regex_filename;
-    std::string table[MAX_COLS][MAX_ROWS];
+    std::string data[MAX_COLS][MAX_ROWS];
     std::string columns[MAX_COLS];
     std::string regex[MAX_COLS];
     std::string line;
@@ -21,34 +32,25 @@ private:
 
     void load_text();
     void load_regex();
-
-public:
-    Table(std::string text_filename, std::string regex_filename) {
-        this->text_filename = text_filename;
-        this->regex_filename = regex_filename;
-        load_text();
-        load_regex();
-
-        columns[0] = "text";
-    }
-
     void print_text(int start, int end);
     void print_columns();
     void print_regex();
     void print_regex_maps();
+    void write_csv(const std::string& csv_filename, const std::string& sep);
+    static std::string match(const std::string& text, const std::string& pattern, bool multivariate);
 };
-
-// PRIVATE METHODS
 
 inline void Table::load_text() {
     std::ifstream text_file(this->text_filename);
 
     if (text_file.is_open()) {
         while (std::getline(text_file, this->line) && this->r < MAX_ROWS) {
-            this->table[0][this->r] = this->line;
+            this->data[0][this->r] = this->line;
             this->r++;
         }
         text_file.close();
+        columns[c] = "TEXT";
+        c++;
     } else {
         std::cout << "Unable to open file.\n";
     }
@@ -57,28 +59,28 @@ inline void Table::load_text() {
 inline void Table::load_regex() {
     std::ifstream regex_file(this->regex_filename);
 
+    int p = 0;
+
     if (regex_file.is_open()) {
         while (std::getline(regex_file, this->line) && this->c < MAX_COLS) {
-            if (this->c % 2 == 0) {
-                this->columns[this->c/2] = this->line;
+            if (p % 2 == 0) {
+                this->columns[c] = this->line;
             } else {
-                this->regex[this->c/2] = this->line;
+                this->regex[c] = this->line;
+                c++;
             }
-            this->c++;
+            p++;
         }
-        this->c = this->c / 2;
         regex_file.close();
     } else {
         std::cout << "Unable to open file.\n";
     }
 }
 
-// PUBLIC METHODS
-
 inline void Table::print_text(int start = 0, int end = MAX_ROWS) {
     if ((start <= end) && (end <= this->r)) {
         for (int i = start; i <= end; i++) {
-            std::cout << this->table[0][i] << "\n\n";
+            std::cout << this->data[0][i] << "\n\n";
         }
     } else {
         std::cout << "Invalid index.\n";
@@ -104,6 +106,61 @@ inline void Table::print_regex_maps() {
         std::cout << columns[i] << ": " << regex[i] << "\n";
     }
     std::cout << "\n";
+}
+
+inline void Table::write_csv(const std::string& csv_filename, const std::string& sep = "|") {
+    std::ofstream csv_file(csv_filename);
+
+    if (csv_file.is_open()) {
+        for (int i = 0; i < c; i++) {
+            csv_file << columns[i];
+            if (i < c-1) {
+                csv_file << sep;
+            }
+        }
+        csv_file << "\n";
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                csv_file << data[j][i];
+                if (j < c-1) {
+                    csv_file << sep;
+                }
+            }
+            csv_file << "\n";
+        }
+    } else {
+        std::cout << "Unable to write file.\n";
+    }
+
+    csv_file.close();
+}
+
+inline std::string Table::match(const std::string& text, const std::string& pattern, bool multivariate = false) {
+    std::regex regex_pattern(pattern);
+    std::smatch match;
+    std::string result;
+
+    if (multivariate) {
+        std::string sep = "|";
+        bool found_match = false;
+
+        auto it = text.begin();
+        while (std::regex_search(it, text.end(), match, regex_pattern)) {
+            result += match.str() + sep;
+            it = match[0].second;
+            found_match = true;
+        }
+        if (found_match) {
+            result.pop_back();
+        }
+    } else {
+        if (std::regex_search(text, match, regex_pattern)) {
+            result = match.str();
+        }
+    }
+
+    return result;
 }
 
 #endif //NERPLUS_TABLE_H
